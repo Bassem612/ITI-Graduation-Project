7c6ce81568c010814e0abea3f0b50583cb3d22b3
Bassem Desouki <bassemdesouqi78@gmail.com>
2022-03-18 17:51:27 +0200

    Fixing: Module not found: Error: Cannot find file: 'AboutUs.js' does not match the corresponding name on disk

--- iti-graduation-project/src/App.js
+++ iti-graduation-project/src/App.js
@@ -1,12 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from "./Components/Nav/Nav";
 // import Slider from "./Components/Slider";
+
+import Nav from "./Components/Nav/Nav";
 import ContactUs from "./Components/Contact/ContactUs";
 import Faq from "./Components/FAQ/Faq";
 import AboutUs from "./Components/About/AboutUs";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Footer from "./Components/Footer/Footer";
 
